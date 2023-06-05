879bdb51cb65f9b5d8e13978533ab84218c88dff
e3palma <91630458+e3palma@users.noreply.github.com>
2023-06-05 06:31:05 -0700

    Add files via upload

--- Scripts/Install_moonlight.sh
+++ Scripts/Install_moonlight.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-
+OS=$(cat /etc/os-release | grep VERSION_CODENAME | awk -F'=' '{printf $2}')
+echo "$OS"
 echo -e "\nAdding Moonlight to Sources List..."
-
-if grep -q "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" /etc/apt/sources.list; then
+## new 
+if grep -q "deb [signed-by=/usr/share/keyrings/moonlight-game-streaming-moonlight-embedded-archive-keyring.gpg] https://dl.cloudsmith.io/public/moonlight-game-streaming/moonlight-embedded/deb/raspbian buster main" /etc/apt/sources.list.d/moonlight-game-streaming-moonlight-embedded.list; then
     echo -e "NOTE: Moonlight Source Exists - Skipping"
 else
     echo -e "Adding Moonlight to Sources List"
-    echo "deb http://archive.itimmer.nl/raspbian/moonlight stretch main" >> /etc/apt/sources.list
-fi
-
-echo -e "\nFetching and installing the GPG key....\n"
-
-if [ -f /home/pi/itimmer.gpg ]
-then	
-    echo -e "NOTE: GPG Key Exists - Skipping"
-else		
-    wget http://archive.itimmer.nl/itimmer.gpg
-    chown pi:pi /home/pi/retromoonlight/itimmer.gpg
-    apt-key add itimmer.gpg		
+    curl -1sLf 'https://dl.cloudsmith.io/public/moonlight-game-streaming/moonlight-embedded/setup.deb.sh' | distro=raspbian codename=$OS sudo -E bash
 fi
 
 echo -e "\nUpdating System..."
@@ -28,18 +18,25 @@ cd ..
 
 sudo apt-get update -y
 
-sudo apt-get install git libopus0 libexpat1 libasound2 libudev1 libavahi-client3 libcurl4 libevdev2 libenet7 libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev cmake gcc g++ fakeroot debhelper -y
-sudo apt-get install libraspberrypi-dev -y
+## no longer needed due to deb package ##
+#sudo apt-get install git libopus0 libexpat1 libasound2 libudev1 libavahi-client3 libcurl4 libevdev2 libenet7 libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev cmake gcc g++ fakeroot debhelper -y
 
-git clone https://github.com/irtimmer/moonlight-embedded.git
-cd moonlight-embedded
-git submodule update --init
-mkdir build
-cd build/
-cmake ../
-make
-sudo make install
-sudo ldconfig
+#sudo apt install rbp-userland-dev-osmc libraspberrypi0 rbp-userland-osmc
+#sudo apt-get install libraspberrypi-dev -y
+
+#git clone https://github.com/irtimmer/moonlight-embedded.git
+#cd moonlight-embedded
+#git submodule update --init
+#fakeroot debian/rules binary
+#mkdir build
+#cd build/
+#cmake ../
+#make
+#sudo make install
+#sudo ldconfig
+
+
+sudo apt install moonlight-embedded
 
 cd retromoonlight
 
